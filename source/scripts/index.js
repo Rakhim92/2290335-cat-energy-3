60134462507b515ca8b1ab6ffa78bbd5383cddfd
Rakhim <pharmamystery19@gmail.com>
2026-08-01 17:24:29 +0700

    Внес правки: работаю со stack.svg, выпадающее меню

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,9 @@
 /* в этот файл добавляет скрипты*/
+
+const burgerBtn = document.querySelector('.page-header__button');
+const headerNavigationContent = document.querySelector('.page-header__navigation');
+
+burgerBtn.addEventListener('click', () => {
+  burgerBtn.classList.toggle('page-header__button--show');
+  headerNavigationContent.classList.toggle('page-header__navigation--show');
+});
