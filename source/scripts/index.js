026a3dc1851aacb20cf91589fbd0a6fa0f503d94
Rakhim <pharmamystery19@gmail.com>
2026-08-08 22:58:47 +0700

    Сверстана index.js mobile-viewport

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,7 +3,14 @@
 const burgerBtn = document.querySelector('.page-header__button');
 const headerNavigationContent = document.querySelector('.page-header__navigation');
 
+const slider = document.querySelector('.slider');
+const range = document.querySelector('.slider__range-js');
+
 burgerBtn.addEventListener('click', () => {
   burgerBtn.classList.toggle('page-header__button--show');
   headerNavigationContent.classList.toggle('page-header__navigation--show');
+});
+
+range.addEventListener('input', () => {
+  slider.style.setProperty('--value', `${range.value }%`);
 });
